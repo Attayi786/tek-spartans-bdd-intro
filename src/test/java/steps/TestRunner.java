ecdd927872e2e2a7a9d2a3586bf3e061290a8360
mydocuments <hamed.mr.unique@gmail.com>
2024-07-29 22:56:50 -0700

    Cucumber intro

--- src/test/java/steps/TestRunner.java
+++ src/test/java/steps/TestRunner.java
@@ -1,4 +1,16 @@
 package steps;
 
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//cucumber-junit 4 implementation
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:feature/MyFirstFeature.feature",
+        glue = "steps"
+)
 public class TestRunner {
 }
